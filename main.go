24e0fc0874e4cbfab1cdb45050f3ecb24ad15681
jchang118 <josephchang911@msn.com>
2026-05-23 21:40:53 +1000

    up to 2.6 completed

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Use the Header().Add() method to add a 'Server: Go' header to the
+	// response header map. The first parameter is the header name, and
+	// the second parrameter is the header value.
+	w.Header().Add("Server", "Go")
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -17,19 +22,27 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)                      // Restrict this route to exact matches on / only.
-	mux.HandleFunc("/snippet/view/{id}", snippetView) // Add the {id} wildcard segment.
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	// Prefix the route patterns with the required HTTP method (for now, we will
+	// restrict all three routes to acting on GET requests.)
+	mux.HandleFunc("GET /{$}", home)                      // Restrict this route to exact matches on / only.
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView) // Add the {id} wildcard segment.
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 
