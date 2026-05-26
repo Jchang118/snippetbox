4471b6a2e49692645848c5a13f5ddc17d39dd140
jchang118 <josephchang911@msn.com>
2026-05-27 00:22:21 +1000

    5.2 completed

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/jchang118/snippetbox/internal/models"
 
+// Include a Snippets field in the templateData struct
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
