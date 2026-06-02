aaec1a4f5e923324bc6afee13930edbb06cf1edf
jchang118 <josephchang911@msn.com>
2026-06-02 16:41:53 +1000

    10.3 completed

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -18,6 +23,28 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	// Create a bcrypt hash of the plain-text password.
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES (?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		// If there is an error, we use the errors.AsType() function to check
+		// whether the error matches the type *mysql.MySQLError, and it if does,
+		// assign it to the mySQLError variable.
+		if mySQLError, ok := errors.AsType[*mysql.MySQLError](err); ok {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+
+		// For all other errors, return them as normal.
+		return err
+	}
 	return nil
 }
 
