33cea8879103bf62e74453a2adee0f32954f8ca0
jchang118 <josephchang911@msn.com>
2026-06-03 14:04:35 +1000

    chapter 12 completed

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"github.com/jchang118/snippetbox/internal/models"
+	"github.com/jchang118/snippetbox/ui"
 )
 
 // Include a Snippets field in the templateData struct
@@ -34,7 +36,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	// match the pattern "./ui/html/pages/*tmpl". This will essentially give
 	// us a slice of all the filepaths for our application 'page' templates
 	// like [ui/html/pages/home.tmp ui/html/pages/view.tmpl]
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
@@ -45,28 +47,21 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// The template.FuncMap must be registered with the template set before you
-		// call the ParseFiles() method. This means we have to use template.New() to
-		// create an empty template set, use the Funcs() method to register the
-		// template.FuncMap, and then parse the file as normal.
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		// Create a slice containing the filepath patters for the template we
+		// want to parse
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+
+		// Use ParseFS() instead of ParseFiles() to parse the template files
+		// from the ui.Files embedded filesystem.
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 
-		// Call ParseGlob() *on this template set* to add any partials.
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
-
-		// Add the template set to the map, using the name of the page
-		// (like 'home.tmpl') as the key.
 		cache[name] = ts
 	}
 
