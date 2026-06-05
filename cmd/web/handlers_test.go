7201e192387022172db86381989d72e1a0905adf
jchang118 <josephchang911@msn.com>
2026-06-05 21:52:07 +1000

    13.4 completed?

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/jchang118/snippetbox/internal/assert"
+)
+
+func TestPing(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	res := ts.get(t, "/ping")
+	assert.Equal(t, res.status, http.StatusOK)
+	assert.Equal(t, res.body, "OK")
+}
